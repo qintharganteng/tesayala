2f8638d814ddc68e9c6c949fa9bc1f118d6b6108
qintharganteng <qintharalmaliki@gmail.com>
2024-10-23 10:18:06 +0700

    Add VSCode settings and Hinter configuration files

---
+++ js/fetch.js
@@ -0,0 +1,6 @@
+import { get } from "https://bukulapak.github.io/api/process.js"; 
+let urlAPI = "https://msg.ulbi.ac.id/task/recruitment/all";
+get(urlAPI,isiTablePresensi);
+function isiTablePresensi(results){
+    console.log(results);
+}
