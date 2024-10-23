0c19f06e4f5d44ae70a3730fdf2ec06de16f22ef
qintharganteng <qintharalmaliki@gmail.com>
2024-10-23 12:21:40 +0700

    Refactor fetch.js to use fetch API for retrieving data from the server, handle error responses, and set appropriate headers

--- js/fetch.js
+++ js/fetch.js
@@ -2,9 +2,16 @@ const endpoint = "https://msg.ulbi.ac.id/task/recruitment/all";
 
 fetch(endpoint, {
     method: 'GET',
-    mode: 'no-cors' 
+    mode: 'cors', // coba tambahkan ini
+    headers: {
+        'Content-Type': 'application/json'
+    }
 })
-.then(response => {
-    console.log(response); 
+.then((response) => {
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.json(); // parsing response ke json
 })
-.catch(error => console.error('Error:', error));
+.then((data) => console.log(data))
+.catch((error) => console.error('Error:', error));
