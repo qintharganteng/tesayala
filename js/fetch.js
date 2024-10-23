05f24679c0c930c100164c6d7523f49816da3b81
qintharganteng <qintharalmaliki@gmail.com>
2024-10-23 10:48:35 +0700

    Refactor fetch.js to use fetch API for retrieving data from the server

--- js/fetch.js
+++ js/fetch.js
@@ -2,13 +2,5 @@ let urlAPI = "https://msg.ulbi.ac.id/task/recruitment/all";
 
 fetch(urlAPI)
   .then(response => response.json())
-  .then(results => isiTablePresensi(results))
+  .then(results => console.log(results))
   .catch(error => console.error('Error:', error));
-
-function isiTablePresensi(results) {
-    console.log(results);
-}
-
-
-
-
