f7244bff2d2b594b89bd9ea9e6687358da6e5b28
qintharganteng <qintharalmaliki@gmail.com>
2024-10-23 19:23:11 +0700

    Refactor fetch.js to use fetch API for retrieving data from the server and fix error handling

--- js/fetch.js
+++ js/fetch.js
@@ -1,13 +1,28 @@
 console.log("FETCH");
 
-const endpoint = "https://msg.ulbi.ac.id/task/recruitment/all";
+  const endpoint = "https://msg.ulbi.ac.id/task/recruitment/all";
 
-fetch(endpoint)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then((result) => console.log(result))
-  .catch((error) => console.error("Fetch error:", error));
+  fetch(endpoint)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((result) => {
+      const tableBody = document.querySelector("#jobTable tbody");
+      result.forEach((job) => {
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
+          <td class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-left">${job.id}</td>
+          <td class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-center">${job.name}</td>
+          <td class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-500 text-left">${job.phone}</td>
+          <td class="whitespace-nowrap px-4 bg-white text-sm font-medium text-green-500 text-left">${job.email}</td>
+          <td class="whitespace-nowrap px-4 bg-white text-sm font-medium text-green-500 text-left">${job.alamat}</td>
+        `;
+
+        tableBody.appendChild(row);
+      });
+    })
+    .catch((error) => console.error("Fetch error:", error));
