5f42b1e07b8a80bec992a74d107559b7a7abd5f3
qintharganteng <qintharalmaliki@gmail.com>
2024-10-23 10:40:18 +0700

    Refactor fetch.js to use fetch API for retrieving data from the server

--- js/fetch.js
+++ js/fetch.js
@@ -1,13 +1,12 @@
-fetch('https://msg.ulbi.ac.id/task/recruitment/all')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('There has been a problem with your fetch operation:', error);
-  });
+//fetch
+import { get } from "https://bukulapak.github.io/api/process.js"; 
+let urlAPI = "https://msg.ulbi.ac.id/task/recruitment/all";
+get(urlAPI,isiTablePresensi);
+function isiTablePresensi(results){
+    console.log(results);
+}
+
+
+
+
+
